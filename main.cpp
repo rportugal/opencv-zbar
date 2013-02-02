f9a3f1c4042bc4de34204c4fe95f474b6edf632c
Ricardo Portugal <ricardofmportugal@gmail.com>
2013-02-03 00:06:19 +0100

    added location of symbols

--- main.cpp
+++ main.cpp
@@ -10,11 +10,13 @@ using namespace cv;
 using namespace zbar;
 
 int main(int argc, char **argv) {
-    VideoCapture cap(0);
+    VideoCapture cap(1);
     if (!cap.isOpened()) {
         cerr << "Could not open camera." << endl;
         exit(EXIT_FAILURE);
     }
+    //cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    //cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 
     namedWindow("captured", CV_WINDOW_AUTOSIZE);
     
@@ -41,26 +43,46 @@ int main(int argc, char **argv) {
         Image image(width, height, "Y800", raw, width * height);
 
         // Scan the image for barcodes
-        int n = scanner.scan(image);
-
-        // Show captured frame
-        imshow("captured", frame);
+        //int n = scanner.scan(image);
+        scanner.scan(image);
 
         // Extract results
+        int counter = 0;
         for (Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol) {
             time_t now;
             tm *current;
             now = time(0);
             current = localtime(&now);
 
-            // do something useful with results                                                                                                      
-            cout    << "[" << current->tm_hour << ":" << current->tm_min << ":" << setw(2) << setfill('0') << current->tm_sec << "] "
-                    << "decoded " << symbol->get_type_name()                                                                                            
-                    << " symbol \"" << symbol->get_data() << '"' << endl;                                                                               
-             }                                                                                                                                            
+            // do something useful with results
+            cout    << "[" << current->tm_hour << ":" << current->tm_min << ":" << setw(2) << setfill('0') << current->tm_sec << "] " << counter << " "
+                    << "decoded " << symbol->get_type_name()
+                    << " symbol \"" << symbol->get_data() << '"' << endl;
+
+            //cout << "Location: (" << symbol->get_location_x(0) << "," << symbol->get_location_y(0) << ")" << endl;
+            //cout << "Size: " << symbol->get_location_size() << endl;
+            
+            // Draw location of the symbols found
+            if (symbol->get_location_size() == 4) {
+                //rectangle(frame, Rect(symbol->get_location_x(i), symbol->get_location_y(i), 10, 10), Scalar(0, 255, 0));
+                line(frame, Point(symbol->get_location_x(0), symbol->get_location_y(0)), Point(symbol->get_location_x(1), symbol->get_location_y(1)), Scalar(0, 255, 0), 2, 8, 0);
+                line(frame, Point(symbol->get_location_x(1), symbol->get_location_y(1)), Point(symbol->get_location_x(2), symbol->get_location_y(2)), Scalar(0, 255, 0), 2, 8, 0);
+                line(frame, Point(symbol->get_location_x(2), symbol->get_location_y(2)), Point(symbol->get_location_x(3), symbol->get_location_y(3)), Scalar(0, 255, 0), 2, 8, 0);
+                line(frame, Point(symbol->get_location_x(3), symbol->get_location_y(3)), Point(symbol->get_location_x(0), symbol->get_location_y(0)), Scalar(0, 255, 0), 2, 8, 0);
+            }
+            
+            // Get points
+            /*for (Symbol::PointIterator point = symbol.point_begin(); point != symbol.point_end(); ++point) {
+                cout << point << endl;
+            } */
+            counter++;
+        }
+
+        // Show captured frame, now with overlays!
+        imshow("captured", frame);
                                                                                                                                                           
-        // clean up                                                                                                                                  
-        image.set_data(NULL, 0); 
+        // clean up
+        image.set_data(NULL, 0);
         
         waitKey(30);
     }
